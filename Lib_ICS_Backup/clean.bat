f86d595ef9d7222770c28baf9917997c748a5c22
김의한 <117137366+KimuihanHC@users.noreply.github.com>
2023-09-21 17:31:21 +0900

    TEST
    TEST

--- Lib_ICS_Backup/clean.bat
+++
@@ -1,2 +0,0 @@
-del /s *.suo;*.sdf;*.ipch;*.obj;*.exp;*.log;*.ilk;*.pch;*.pdb;*.idb;*.tlog;*.lastbuildstate;*.tlh;*.tli;*.aps
-del /s /a:h *.suo;*.sdf;*.ipch;*.obj;*.exp;*.log;*.ilk;*.pch;*.pdb;*.idb;*.tlog;*.lastbuildstate;*.tlh;*.tli;*.aps
